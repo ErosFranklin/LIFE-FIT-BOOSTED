a9037e0f0ac302319589a1b9dd4367d1c524e7fa
Francisco Neto <medeirosfrancisco0407@gmail.com>
2025-04-12 13:03:51 -0300

    Feature/4 back end registration (#11)
    * Express amd bcrypt instaled
    
    * added mongodb configs and initial express config
    
    * added user model
    
    * added user controller and logger
    
    * added user routes and some bugs resolved

---
+++ backend/controllers/user_controllers.js
@@ -0,0 +1,59 @@
+const User = require('../models/user.js');
+const logger = require('../utils/logger.js');
+
+exports.createUserController = async (data) => {
+    const { name, email, password, number, weight, height } = data;
+
+    try {
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return { error: 'Email já está em uso', statusCode: 400 }; // Retornando como objeto
+        }
+        
+        const newUser = new User({
+            name,
+            email,
+            password,
+            number, 
+            weight, 
+            height
+        });
+
+        await newUser.save();
+
+        logger.info(`Usuário de nome ${newUser.name} e email ${newUser.email} criado com sucesso!`);
+        return { message: 'Usuário criado com sucesso', user: newUser, statusCode: 201 }; 
+    } catch (err) {
+        logger.error(`Erro ao criar usuário: ${err.message}`);
+        return { error: 'Erro interno do servidor', statusCode: 500 };
+    }
+};
+
+// Buscar usuário autenticado por ID
+exports.getAuthenticatedUser = async (req, res) => {
+    const userId = req.params.id;
+
+    if (!mongoose.Types.ObjectId.isValid(userId)) {
+        return res.status(400).json({ message: 'ID inválido' });
+    }
+
+    if (!req.user || req.user._id.toString() !== userId) {
+        logger.warn(`Acesso negado: usuário não autenticado ou ID inválido.`);
+        return res.status(403).json({ message: 'Acesso negado: Usuário não autenticado ou ID inválido' });
+    }
+
+    try {
+        const user = await User.findById(userId).select('-password');
+
+        if (!user) {
+            logger.warn(`Usuário de ID ${userId} não encontrado.`);
+            return res.status(404).json({ message: 'Usuário não encontrado' });
+        }
+
+        logger.info(`Usuário de ID ${userId} encontrado com sucesso.`);
+        res.status(200).json(user);
+    } catch (err) {
+        logger.error(`Erro ao buscar usuário: ${err.message}`);
+        res.status(500).json({ message: 'Erro ao buscar usuário: ' + err.message });
+    }
+};
---
+++ backend/models/mongo/mongo.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('Conectado ao MongoDB');
+    } catch (err) {
+        console.error('Erro ao conectar ao MongoDB:', err);
+        process.exit(1); 
+    }
+};
+
+module.exports = connectDB;
