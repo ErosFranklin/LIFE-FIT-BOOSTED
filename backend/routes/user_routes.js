389861eea7b50f9a46fe3859cce14b8dfa8a6582
ImFranciscoNeto <medeirosfrancisco0407@gmail.com>
2025-04-14 21:16:42 -0300

    added birthday day and correction routes

--- backend/routes/user_routes.js
+++ backend/routes/user_routes.js
@@ -3,15 +3,15 @@ const { createUserController } = require('../controllers/user_controllers.js');
 
 const router = express.Router();
 
-router.post('/users', async (req, res) => {
+router.post('/user/register', async (req, res) => {
     const data = req.body;
 
-    if (!data || !data.name || !data.email || !data.password || !data.number || !data.weight || !data.height) {
+    if (!data || !data.name || !data.email || !data.password || !data.birthday_day || !data.number || !data.weight || !data.height) {
        return res.status(400).json({ error: 'Todos os campos são obrigatórios' });
    }
 
    const result = await createUserController(data);
     return res.status(result.statusCode || 500).json(result);
-});
+}); 
 
 module.exports = router;
