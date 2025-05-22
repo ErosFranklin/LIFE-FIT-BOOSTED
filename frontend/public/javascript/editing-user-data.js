b3008fe4eea0f0b5c15abb4921b112e2d44cc933
Eros Franklin <eros.felix@aluno.uepb.edu.br>
2025-05-22 15:47:17 -0300

    update: user datas inm editing page

---
+++ frontend/public/javascript/editing-user-data.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function () {
+    getUserData();
+
+
+    async function getUserData() {
+        const token = localStorage.getItem("token");
+        if (!token) {
+            console.error("Token não encontrado.");
+            return;
+        }
+
+        try {
+            const response = await fetch(`http://localhost:10000/api/data_user`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`,
+            },
+        });
+
+        if (!response.ok) {
+            throw new Error("Erro ao obter dados do usuário.");
+        }
+
+            const data = await response.json();
+            console.log(data);
+            document.querySelector(".name-user").value = data.user.name;
+            document.querySelector(".bithday-user").value = new Date(data.user.birthday_day).toISOString().split("T")[0];
+            document.querySelector(".email-user").value = data.user.email;
+            document.querySelector(".phone-user").value = data.user.number;
+            document.querySelector(".height-user").value = data.user.height;
+            document.querySelector(".weight-user").value = data.user.weight;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    
+
+})
