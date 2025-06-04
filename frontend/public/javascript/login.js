9ff8c014e3046be8ca6f9862d27371c7c6b14553
Eros Franklin <148378994+ErosFranklin@users.noreply.github.com>
2025-06-03 22:17:12 -0300

    Hotfix/86 bug path directory (#88)
    * Feature/68 frontend trainning visualization (#69)
    
    * update: changes in trainning pages
    
    * logger
    
    * Update info.log
    
    * Feature/24 frontend interface user data (#70)
    
    * user data interface
    
    * style: style on user data page
    
    * update: add logic user data
    
    * style: change style
    
    * Feature/25 backend user data editing (#71)
    
    * Added logical equipament of training
    
    * Added user data editing
    
    * update in the logic of editing user data
    
    * Feature/28 frontend user data editing interface (#74)
    
    * creation page editing user data
    
    * style: applying style to the edit user data page
    
    * update: user datas inm editing page
    
    * update split trainning in page user data
    
    * update:logic editing user data
    
    * Feature/25 backend user data editing (#71) (#72)
    
    * Added logical equipament of training
    
    * Added user data editing
    
    * update in the logic of editing user data
    
    Co-authored-by: Francisco Neto <medeirosfrancisco0407@gmail.com>
    
    * update: editing page
    
    ---------
    
    Co-authored-by: Francisco Neto <medeirosfrancisco0407@gmail.com>
    
    * Feature/25 backend user data editing (#75)
    
    * Added logical equipament of training
    
    * Added user data editing
    
    * update in the logic of editing user data
    
    * Update on user data update
    
    * Feature/73 frontend dashboard (#77)
    
    * update: dashboard
    
    * delete: code desnecessary
    
    * Feature/73 frontend dashboard (#78)
    
    * update: dashboard
    
    * delete: code desnecessary
    
    * update: route error
    
    * create .env example
    
    * update: editing trainning (#81)
    
    * update: editing trainning
    
    * Bugfix/1 backend controllers (#82)
    
    * Added logical equipament of training
    
    * Added user data editing
    
    * update in the logic of editing user data
    
    * Update on user data update
    
    * Update in controller training
    
    * update in user controller
    
    ---------
    
    Co-authored-by: ImFranciscoNeto <medeirosfrancisco0407@gmail.com>
    
    * Feature/79 frontend interface editing trainnings (#84)
    
    * updates (#83)
    
    * update: editing trainning (#81)
    
    * Bugfix/1 backend controllers (#82)
    
    * Added logical equipament of training
    
    * Added user data editing
    
    * update in the logic of editing user data
    
    * Update on user data update
    
    * Update in controller training
    
    * update in user controller
    
    ---------
    
    Co-authored-by: ImFranciscoNeto <medeirosfrancisco0407@gmail.com>
    
    ---------
    
    Co-authored-by: ImFranciscoNeto <medeirosfrancisco0407@gmail.com>
    
    * update info.log
    
    * update: logic editing trainning
    
    * update: path for vercel
    
    ---------
    
    Co-authored-by: ImFranciscoNeto <medeirosfrancisco0407@gmail.com>
    
    * bug:path directory
    
    * bug: note deploy
    
    * update info.llog
    
    * update info.log
    
    ---------
    
    Co-authored-by: Francisco Neto <medeirosfrancisco0407@gmail.com>

--- frontend/public/javascript/login.js
+++ frontend/public/javascript/login.js
@@ -23,7 +23,7 @@ document.addEventListener("DOMContentLoaded", async function () {
         }
 
         try {
-            const response = await fetch("https://life-fit-boosted.vercel.app/api/user/login", {
+            const response = await fetch("http://localhost:10000/api/user/login", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
@@ -45,7 +45,7 @@ document.addEventListener("DOMContentLoaded", async function () {
                 localStorage.setItem("token-decode", decode);
                 localStorage.setItem("trainning-split", data.user.training_split);
             }
-            window.location.href = "/views/trainning.html"; 
+            window.location.href = "../public/views/trainning.html"; 
 
         } catch (error) {
             console.error("Erro ao fazer login:", error);
