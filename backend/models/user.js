97077d9f0d675c0bd1ce977583cc7087b6d1222a
Eros Franklin <eros.felix@aluno.uepb.edu.br>
2025-06-03 21:55:23 -0300

    update: logic editing trainning

--- backend/models/user.js
+++ backend/models/user.js
@@ -54,6 +54,7 @@ const userSchema = new mongoose.Schema({
             exercise: {
               type: [
                 {
+                   _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
                   name: { type: String, required: true },
                   series: { type: Number, required: true },
                   equipment: { type: String, required: true }
@@ -61,7 +62,7 @@ const userSchema = new mongoose.Schema({
               ],
               validate: {
                 validator: function (val) {
-                  return val.length >= 2 && val.length <= 5;
+                  return val.length >= 1 && val.length <= 5;
                 },
                 message: 'Cada grupo muscular deve ter entre 2 e 5 exercícios.'
               }
